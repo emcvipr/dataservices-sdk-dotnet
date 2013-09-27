ba21aafc655fd2359994018724d71712191f82e6
Eron Wright <ewright@live.com>
2013-09-27 14:12:05 -0700

    Add MSI for ViPR SDK

--- pack.cmd
+++ pack.cmd
@@ -1,11 +1,8 @@
 @echo off
 @setlocal
 
-mkdir bin 2> nul
-mkdir bin\Debug 2> nul
-mkdir bin\Release 2> nul
-
 SET SOLUTIONDIR=%~dp0
+SET PATH=%ProgramFiles%\WiX Toolset v3.7\bin;%PATH%
 
 SET VERSION=%1
 IF "%VERSION%" NEQ "" GOTO versionset
@@ -17,4 +14,22 @@ IF "%CONFIGURATIONNAME%" NEQ "" GOTO configurationnameset
 SET CONFIGURATIONNAME=Release
 :configurationnameset
 
+CD %SOLUTIONDIR%
+
+mkdir bin 2> nul
+mkdir bin\Debug 2> nul
+mkdir bin\Release 2> nul
+mkdir obj 2> nul
+mkdir obj\Debug 2> nul
+mkdir obj\Release 2> nul
+
+REM MSI
+candle ViPR.wxs -o obj\%ConfigurationName%\ViPR.wixobj
+candle AWSSDK\AWSSDK.wxs -o obj\%ConfigurationName%\AWSSDK.wixobj -dAWSSDKSrc.TargetDir=%SOLUTIONDIR%\AWSSDK\bin\%ConfigurationName%
+candle AWS.Extensions\SessionProvider\AWS.SessionProvider.wxs -o obj\%ConfigurationName%\AWS.SessionProvider.wixobj -dSessionProvider.TargetDir=%SOLUTIONDIR%\AWS.Extensions\SessionProvider\bin\%ConfigurationName%
+candle AWS.Extensions\TraceListener\AWS.TraceListener.wxs -o obj\%ConfigurationName%\AWS.TraceListener.wixobj -dTraceListener.TargetDir=%SOLUTIONDIR%\AWS.Extensions\TraceListener\bin\%ConfigurationName%
+
+light -o bin\%ConfigurationName%\ViPRDataServicesSDK.%Version%.msi -pdbout obj\%ConfigurationName%\ViPRDataServicesSDK.%Version%.wixpdb obj\%ConfigurationName%\ViPR.wixobj obj\%ConfigurationName%\AWSSDK.wixobj obj\%ConfigurationName%\AWS.SessionProvider.wixobj obj\%ConfigurationName%\AWS.TraceListener.wixobj
+
+REM NUGET
 %SOLUTIONDIR%nuget pack %SOLUTIONDIR%package.nuspec -OutputDirectory %SOLUTIONDIR%bin\%CONFIGURATIONNAME% -Version %VERSION% -Properties "ConfigurationName=%CONFIGURATIONNAME%"
